2aaf1b4a77d5dd4a5f9fee13eebd2e78b7d3ed6b
xavier-castro <xavier_castro@icloud.com>
2019-05-31 08:20:17 -0500

    Add data file and horizontal scroll view

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import './Custom_Icons.dart';
+import './data.dart';
 
 void main() => runApp(MaterialApp(
       home: MyApp(),
@@ -28,12 +30,40 @@ class _MyAppState extends State<MyApp> {
 
   Widget _buildAppBar() {
     return Positioned(
-        top: 40.0,
-        left: 20.0,
-        right: 20.0,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[IconButton()],
+      top: 40.0,
+      left: 20.0,
+      right: 20.0,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          IconButton(
+            icon: Icon(CustomIcons.menu, color: Colors.black),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: Icon(CustomIcons.search, color: Colors.black),
+            onPressed: () {},
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _buildTitle(double height) {
+    return Positioned(
+        top: height * .2,
+        left: 30.0,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text("Wooden Armchair",
+                style:
+                    TextStyle(fontSize: 28.0, fontFamily: "Montserrat-Bold")),
+            Text(
+              "Lorem Ipsem",
+              style: TextStyle(fontSize: 16.0, fontFamily: "Montserrat-Medium"),
+            )
+          ],
         ));
   }
 
@@ -50,7 +80,48 @@ class _MyAppState extends State<MyApp> {
             fit: StackFit.expand,
             children: <Widget>[
               _buildGradientContainer(width, height),
-              _buildAppBar()
+              _buildAppBar(),
+              _buildTitle(height),
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: SizedBox(
+                    height: height * .6,
+                    child: ListView.builder(
+                      itemCount: images.length,
+                      scrollDirection: Axis.horizontal,
+                      physics: BouncingScrollPhysics(),
+                      itemBuilder: (context, index) {
+                        return Padding(
+                          padding: EdgeInsets.only(left: 35.0, bottom: 60.0),
+                          child: SizedBox(
+                            width: 200.0,
+                            child: Stack(
+                              fit: StackFit.expand,
+                              children: <Widget>[
+                                Padding(
+                                  padding: EdgeInsets.only(top: 45.0),
+                                  child: Container(
+                                    decoration: BoxDecoration(
+                                        color: (index % 2 == 0)
+                                            ? Colors.white
+                                            : Color(0xFF2a2d3f),
+                                        boxShadow: [
+                                          BoxShadow(
+                                              color: Colors.black12,
+                                              offset: Offset(0.0, 10.0),
+                                              blurRadius: 10.0)
+                                        ],
+                                        borderRadius:
+                                            BorderRadius.circular(12.0)),
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                        );
+                      },
+                    )),
+              )
             ],
           );
         },
